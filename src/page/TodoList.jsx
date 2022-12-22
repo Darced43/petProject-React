9e538808b9f4f6c63040d1c922b66159962b4c83
Кирилл Лапихин <k19-92@mail.ru>
2022-12-22 19:44:43 +0300

    new edits

--- src/page/TodoList.jsx
+++ src/page/TodoList.jsx
@@ -2,10 +2,10 @@ import React from "react"
 import { useState } from "react"
 import '../app.scss'
 import { FormPost } from "../components/FormPost"
+import TodoTask from "../components/TodoTask"
 import Modal from "../components/UI/modal/Modal"
 
 export const TodoList = () => {
-    let count = 0
     const [IshowModal, setShowModal] = useState(false)
     const [IListName, setListName] = useState([])
 
@@ -24,14 +24,12 @@ export const TodoList = () => {
             }
             <div style={{borderRight:'2px solid rgb(254 226 226)'}} className='ml-2'>
                 <h1>Список задач</h1>
-                {IListName.map( item => {
-                    return <div key={count += 1} className='todo__name'>
-                                <div className="todo__circul"></div>
-                                {item.name}
-                            </div>
-                })}
+                {IListName.map( post => 
+                    <TodoTask post={post} key={post.id} IListName={IListName} setListName={setListName}/>
+                )}
                 <button onClick={() => setShowModal(true)}>+</button>
             </div>
+            {console.log(IListName)}
         </section>
     )
 }
