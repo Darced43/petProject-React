f87560f9686d66758009307ff6851fef1f04bab3
Кирилл Лапихин <k19-92@mail.ru>
2022-12-28 14:52:02 +0300

    use undexedDB

--- src/components/TodoTask.jsx
+++ src/components/TodoTask.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function TodoTask({post, IListName, setListName}){
-    function removeTodo(){
-        console.log(IListName)
-        setListName(IListName.filter(elem => elem.id !== post.id))
+function TodoTask({post, IListName, setListName, deleteNote}){
+    function removeTodo(event){
+        setListName(IListName.filter(elem => elem.id !== post.id));
+        deleteNote(event)
     }
 
     return(
@@ -12,7 +12,7 @@ function TodoTask({post, IListName, setListName}){
                 <div className="todo__circul"></div>
                 {post.name}
             </div>
-            <span style={{cursor: 'pointer', marginRight: '10px'}} onClick={() => removeTodo()}>удалить</span>
+            <span style={{cursor: 'pointer', marginRight: '10px'}} onClick={(event) => removeTodo(event)}>удалить</span>
         </div>
     )
 }
