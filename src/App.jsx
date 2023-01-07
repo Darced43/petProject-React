c227add64a86cf7563ec748fa0b3e793a2786ad5
Кирилл Лапихин <k19-92@mail.ru>
2023-01-07 16:42:24 +0300

    edits magazin

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { NavBar } from './components/NavBar'
 import { TodoList } from './page/TodoList'
 import { Wether } from './page/Wether'
 import { Magazin } from './page/Magazin'
+import ItemProd from './components/ItemProd'
 import './App.scss'
 
 
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path='/' element={<TodoList/>}/>
       <Route path="/magazin" element={<Magazin/>}/>
+      <Route path="/magazin/:id" element={<ItemProd/>}/>
       <Route path="/wether" element={<Wether/>}/>
     </Routes>
     </>
